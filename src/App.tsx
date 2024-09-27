8baa042c92752f1998a97250ca3ded9603da5aa9
Matteo Gobbo <info@matteogobbo.it>
2024-09-27 21:55:17 +0200

    chore: install and configure biome

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
-  return (
-    <h1>React with Extra Powers</h1>
-  )
+	return <h1>React with Extra Powers</h1>;
 }
 
-export default App
+export default App;
