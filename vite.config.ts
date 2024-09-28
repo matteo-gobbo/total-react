cff6272bf37b2b78bdb76b0c3264c9879daeb9c8
Matteo Gobbo <info@matteogobbo.it>
2024-09-28 14:04:52 +0200

    chore: install and configure vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
@@ -5,4 +6,15 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite(), react()],
+  test: {
+    include: ["src/**/*.unit.test.ts"],
+    reporters: ["json", "default"],
+    outputFile: "./test-output.json",
+  },
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+  },
 });
