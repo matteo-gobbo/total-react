ac2c2ac235b699d09bbdb4a13ee6683f7b4964e0
Matteo Gobbo <info@matteogobbo.it>
2024-09-28 12:17:06 +0200

    chore: install and configure react-query

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./queryClient.ts";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 // biome-ignore lint/style/noNonNullAssertion: this is fine in a Vite app
 createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </StrictMode>
 );
